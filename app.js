e9b59db415f9a0ed2ae822e83a6814343d9579ac
Srinivas <srinivas.guruswamy@genworkshealth.com>
2022-03-08 22:19:54 +0530

    Node behind the seen

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const server = http.createServer((req, res) => {
     // Header: ${req.headers}
     // Method: ${req.method}`);
 
-    if(req.url === '/'){
+    if (req.url === '/') {
         res.write(`<HTML> 
             <HEAD> 
             <TITLE>
@@ -21,11 +21,23 @@ const server = http.createServer((req, res) => {
             </form>
             </BODY>
             </HTML>`);
-    return res.end();
+        return res.end();
     }
-    if(req.url === '/message' && req.method === 'POST'){
-fs.writeFile('msg.txt', 'DUMMY');
+    if (req.url === '/message' && req.method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            body.push(chunk);
+        });
 
+        return req.on('end', () => {
+            const postMessage = Buffer.concat(body).toString();
+            const msg = postMessage.split('=')[1];
+            fs.writeFile('msg.txt', msg, (err) => {
+                res.statusCode = 302;
+                res.setHeader('location', '/');
+                return res.end();                
+            });
+        });
     }
 
     res.setHeader('Content-Type', 'text/html');
